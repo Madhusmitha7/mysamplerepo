c431e34d32d1f0f630473b09b9937dd35a1278c1
Madhusmitha7 <konda.madhusmitha@gmail.com>
2022-12-21 17:00:20 +0530

    Hello

---
+++ Waits/explicitwait.java
@@ -0,0 +1,33 @@
+package Waits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class explicitwait {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		driver =new ChromeDriver();
+		
+		driver.manage().window().maximize() ;	
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+		element.sendKeys("Admin");
+		
+		
+		
+		//Login
+		//driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }
+}
